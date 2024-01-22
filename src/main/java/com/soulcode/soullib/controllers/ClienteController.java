a038dbadf19d5e625c66f7c4a7bf2fecdd95ebc7
Carlos Aruh <carlos.aruh@gmail.com>
2024-01-22 16:49:23 -0300

    add livros

--- src/main/java/com/soulcode/soullib/controllers/ClienteController.java
+++ src/main/java/com/soulcode/soullib/controllers/ClienteController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.soulcode.soullib.models.Cliente;
@@ -51,6 +53,37 @@ public class ClienteController {
             return mvErro;
         }
 
+    }
+
+    @PostMapping("/clientes/delete") // action, method, name
+    public String deleteCliente(@RequestParam Integer id) { // GET-POST-REDIRECT
+        // @RequestParam no POST = vai procurar o valor com o nome determinado
+        try {
+            clienteRepository.deleteById(id);
+        } catch (Exception e) {
+            // Em caso de algum problema mostra a página de erro
+            return "erro";
+        }
+
+        // Redireciona o usuário para a lista de clientes
+        // após a remoção feita com sucesso
+        return "redirect:/clientes";
+
+    }
+
+    // inserção
+
+    @PostMapping("/clientes/create")
+    public String createCliente(Cliente cliente) {
+        // @RequestBody => monta o objeto de acordo com os dados vindos
+        // do formulário de requisição (corpo)
+        try {
+            clienteRepository.save(cliente);
+        } catch (Exception e) {
+            return "erro";
+        }
+
+        return "redirect:/clientes";
     }
 
 }
